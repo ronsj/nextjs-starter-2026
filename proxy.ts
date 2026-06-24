7fd7c5a9ccbc23c44b38bf52e061e857292fdce5
Ronald San Jose <ronsj1@gmail.com>
2026-06-24 12:03:51 -0700

    Revert proxy CSP changes

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,6 @@
 import { getSessionCookie } from 'better-auth/cookies'
 import { NextRequest, NextResponse } from 'next/server'
 
-import {
-  buildContentSecurityPolicy,
-  createNonce,
-  getContentSecurityPolicyHeaderKey,
-} from '@/app/lib/content-security-policy'
-
 const protectedPaths = ['/account']
 
 export function proxy(request: NextRequest) {
@@ -19,33 +13,9 @@ export function proxy(request: NextRequest) {
     return NextResponse.redirect(new URL('/sign-in', request.url))
   }
 
-  const nonce = createNonce()
-  const contentSecurityPolicy = buildContentSecurityPolicy(nonce)
-  const cspHeaderKey = getContentSecurityPolicyHeaderKey()
-
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('x-nonce', nonce)
-  requestHeaders.set(cspHeaderKey, contentSecurityPolicy)
-
-  const response = NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  })
-  response.headers.set(cspHeaderKey, contentSecurityPolicy)
-
-  return response
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: [
-    {
-      source:
-        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
-  ],
+  matcher: ['/account', '/account/:path*'],
 }
